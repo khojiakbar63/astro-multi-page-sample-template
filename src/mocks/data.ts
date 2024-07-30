498663fd0ffc79c957baab803303d7cb1e6a96c2
khojiakbar63 <abdulaxatovx7@gmail.com>
2024-07-30 22:06:53 +0500

    astro project

---
+++ src/mocks/data.ts
@@ -0,0 +1,49 @@
+import type { TdbType } from "@type/index"
+
+export const db:TdbType[] = [
+    {
+        id: 1,
+        img: "https://picsum.photos/id/104/600/300",
+        title: "Goose ipsum tiost",
+        description: "Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.",
+        status:true
+    },
+    {
+        id: 2,
+        img: "https://picsum.photos/id/165/600/300",
+        title: "Coose ipsum tiost",
+        description: "Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.",
+        status:true
+    },
+    {
+        id: 3,
+        img: "https://picsum.photos/id/106/600/300",
+        title: "Brand ipsum tiost",
+        description: "Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.",
+        status:true
+    },
+    {
+        id: 4,
+        img: "https://picsum.photos/id/107/600/300",
+        title: "Coose Calcus Trigus",
+        description: "Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.",
+        status:false
+    },
+    {
+        id: 5,
+        img: "https://picsum.photos/id/108/600/300",
+        title: "Coose Calcus Trigus",
+        description: "Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.",
+        status:true
+    },
+    {
+        id: 6,
+        img: "https://picsum.photos/id/109/600/300",
+        title: "Coose Calcus Trigus",
+        description: "Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.",
+        status:true
+    },
+]
+
+
+export const PostsBaseURL = 'https://jsonplaceholder.typicode.com'
---
+++ src/types/index.ts
@@ -0,0 +1,7 @@
+export interface TdbType{
+    id?: number,
+    title: string,
+    description: string,
+    img: string,
+    status: boolean,
+}
